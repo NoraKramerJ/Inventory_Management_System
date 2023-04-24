61037dd1c31629cddb5adcb1e2dc6d8b28f7c1de
Nmerkel <noramerkel0@gmail.com>
2023-04-24 16:02:19 -0400

    Initial commit

---
+++ src/test/java/com/cydeo/inventory_management_system/InventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.inventory_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
